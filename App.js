8f7713d1f46b2a04d0753651935031db8fb247ec
shreyadhaga <shreyadhaga@gmail.com>
2024-05-23 15:14:37 +0530

    nested childred

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 // creating an element is job of reactDOM
 // second parameter of createElement keeps the attributes to a tab
 // third parameter 
+
 const heading = React.createElement("h1", { id: "heading" }, "Hello World from React!");
-console.log(heading)
-// heading is an javascript object
+const child  = React.createElement("div", { id: "child" }, heading);
+const parent  = React.createElement("div", { id: "parent" }, child);
 
 // creating a root is job of reactDOM, this ensures that heading gets converted to h1 tag
 const root = ReactDOM.createRoot(document.getElementById('root'))
