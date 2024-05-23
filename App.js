b34001fe06de53480070ad166b5e5e3640c30658
shreyadhaga <shreyadhaga@gmail.com>
2024-05-23 15:11:06 +0530

    bifurcation of code

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
-  // creating an element is job of reactDOM
-  const heading = React.createElement("h1" , {}, "Hello World from React!");
-  // creating a root is job of reactDOM
-  const root  =  ReactDOM.createRoot(document.getElementById('root'))
-  root.render(heading)
+// creating an element is job of reactDOM
+// second parameter of createElement keeps the attributes to a tab
+// third parameter 
+const heading = React.createElement("h1", { id: "heading" }, "Hello World from React!");
+console.log(heading)
+// heading is an javascript object
+
+// creating a root is job of reactDOM, this ensures that heading gets converted to h1 tag
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(heading)
