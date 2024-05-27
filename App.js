268d84675d69c427dacf13a988a79236dad64eca
shreyadhaga <shreyadhaga@gmail.com>
2024-05-27 16:28:51 +0530

    intro to JSX

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
-// creating an element is job of reactDOM
-// second parameter of createElement keeps the attributes to a tab
-// third parameter can be a single child or an array of chiidren 
 
-const heading = React.createElement("h1", { id: "heading" }, "I am h1 tag!");
-const subheaading = React.createElement("h2", { id: "subheaading" }, "I am h2 tag!");
-const child  = React.createElement("div", { id: "child" }, [heading, subheaading]);
-const parent  = React.createElement("div", { id: "parent" }, child);
+// React Element
+const heading = React.createElement('h1', { id: 'heading' }, 'Hello World')
 
-// creating a root is job of reactDOM, this ensures that heading gets converted to h1 tag
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(parent)
+// JSX - Javascript Syntax to make reaact simpler to write its Syntaxtical Sugar it just ensures that code is like HTML/XML Syntax
+
+const jsxHeading = <h1> Hello JSX </h1>
+// jsxHeading is an react element
+// parcel uses babel to transpile jsx to js which is understandable by browsers 
+// to write JSX in multiple lines its mandatory to wrap it in ()
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(jsxHeading)
