168fd5e44aaabd671f6e67d145bd672e702c2508
shreyadhaga <shreyadhaga@gmail.com>
2024-07-10 11:30:27 +0530

    Well more about cleanups and timers

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,22 +4,31 @@ class UserClass extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 0,
+            userInfo: {
+                name: '',
+                location: '',
+                html_url: '',
+                login: '',
+                avatar_url: ''
+            },
         };
     }
-    componentDidMount(){
-        
+    async componentDidMount() {
+        const data = await fetch('https://api.github.com/users/shreyadhaga');
+        const json = await data.json();
+        this.setState({
+            userInfo: json
+        })
+        console.log(json)
     }
     render() {
-        const { name, location } = this.props;
-        const { count } = this.state;
+        const { name, location, html_url, login, avatar_url } = this.state.userInfo;
         return (
             <div className="user-card">
-                <h1> Count :{count}</h1>
-                <button onClick={() => { this.setState({ count: count + 1 }) }}> Count Increase</button>
+                <img alt='avtar url' src={`${avatar_url}`} />
                 <h2> Name : {name} </h2>
                 <h3> Location: {location} </h3>
-                <h4> Twitter: @wombatmerry</h4>
+                <h4> Github: <a href={`${html_url}`} target="_blank"> {login} </a></h4>
             </div>
         )
     }
